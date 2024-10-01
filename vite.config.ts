fb6c37ad7703e35282708268fba4731a3485dc43
V <username@example.com>
2024-10-01 18:44:24 +0500

    Workflows2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tontastic-wallet/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
